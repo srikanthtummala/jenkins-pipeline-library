ca8e5eb4aed8c3610fd6f1636a7cbb704be297db
SrikanthTummala <srikanth.soa@gmail.com>
2019-02-10 15:26:58 +0100

    Updated Jen script in groovy

--- vars/jen.groovy
+++ vars/jen.groovy
@@ -1,10 +1,11 @@
+def CUSTOM_WORK_SPACE = "F:\\Jenkins_Workspace\\" +  "${env.JOB_NAME}" + "\\" + "${env.BUILD_NUMBER}"
 
-
-def String CUSTOM_WORK_SPACE;
-
+/*
 def call() {
-    CUSTOM_WORK_SPACE = "F:\\Jenkins_Workspace\\" +  "${env.JOB_NAME}" + "\\" + "${env.BUILD_NUMBER}"
+    //CUSTOM_WORK_SPACE = "F:\\Jenkins_Workspace\\" +  "${env.JOB_NAME}" + "\\" + "${env.BUILD_NUMBER}"
     println(CUSTOM_WORK_SPACE)
 }
 
 return this;
+
+*/
